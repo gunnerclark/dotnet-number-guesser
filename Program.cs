2ed73d3c203abb87b52c45c06621c206664f6025
inept <63522725+theineptdev@users.noreply.github.com>
2021-06-01 10:50:18 -0500

    complete

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+// Namespace
+namespace dotnet_number_guesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            GetAppInfo(); // Run GetAppInfo function
+
+            GreetUser(); // Ask for users name and greet
+
+            while (true)
+            {
+
+                // Init correct number
+                // int correctNumber = 7;
+
+                // Create a new Random object
+                Random random = new Random();
+
+                // Init correct number
+                int correctNumber = random.Next(1, 10);
+
+                // Init guess var
+                int guess = 0;
+
+                // Ask user for number
+                Console.WriteLine("Guess a number between 1 and 10");
+
+                // While guess is not correct
+                while (guess != correctNumber)
+                {
+                    // Get user input
+                    string input = Console.ReadLine();
+
+                    // Make sure its a number
+                    if (!int.TryParse(input, out guess))
+                    {
+                        // Tell user its not a number
+                        PrintColorMessage(ConsoleColor.Red, "Please use an actual number");
+
+                        // Keep going
+                        continue;
+                    }
+
+                    // Cast to int and put in guess
+                    guess = Int32.Parse(input);
+
+                    // Match guess to correct number
+                    if (guess != correctNumber)
+                    {
+                        // Tell user its the wrong number
+                        PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again");
+                    }
+                }
+
+                // Output success message - tell user its the right number
+                PrintColorMessage(ConsoleColor.Yellow, "You are correct!");
+
+                // Ask to play again
+                Console.WriteLine("Play Again? (Y or N)");
+
+                // Get answer
+                string answer = Console.ReadLine().ToUpper();
+
+                if(answer == "Y")
+                {
+                    continue;
+                }
+                else if(answer == "N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        // Get and display app info
+        static void GetAppInfo() {
+            // Set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Gunner Clark";
+
+            // Write out app info
+            PrintColorMessage(ConsoleColor.Green, $"{appName}: Version {appVersion} by {appAuthor}");
+        }
+
+        // Ask users name and greet
+        static void GreetUser()
+        {
+            // Ask users name
+            Console.WriteLine("What is your name?");
+
+            // Get user input
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", inputName);
+        }
+
+        // Print color message
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            // Change text color
+            Console.ForegroundColor = color;
+
+            // Write message
+            Console.WriteLine(message);
+
+            // Reset text color - set to white
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# dotnet-number-guesser
-Console-based number guessing app built in .NET Framework
+# [dotnet number guesser](https://www.youtube.com/watch?v=GcFJjpMFJvI&t=1234s)
+
+A console-based number guessing app. Built by following [Traversy Media](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA)'s [C# .NET App tutorial](https://www.youtube.com/watch?v=GcFJjpMFJvI&t=1234s). .
+
+Created to learn [.NET Framework](https://dotnet.microsoft.com/download/dotnet-framework).
+
+<!--
+# Screenshots
+
+ <p align="center">
+  <img src="screenshots/placeholder" width="49%">
+  <img src="screenshots/placeholder" width="49%">
+</p> -->
+
+# Technology
+
+Written in C# using Visual Studio 2019.
